9b6b22094da7d3bf45c9af5534e75af500821c13
HeSig <henrik.sigeman@hotmail.se>
2021-07-05 18:21:20 +0200

    done.php is finished

--- done.php
+++ done.php
@@ -1,1 +1,14 @@
-Participants registered complete
+<html>
+<head>
+  <title>Registered</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="home.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<body>
+  <h1>Registration complete!</h1>
+  <br>
+  <a href="home.php"> Click here to return to registration page </a>
+</body>
+</html>
